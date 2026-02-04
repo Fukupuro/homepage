f8e4f52f622ddb88e6d0fc9d7b13cd634fc657f8
itojum <itojum1230@gmail.com>
2026-02-05 00:53:53 +0900

    feat: Fukupuro -> Fukupro

--- astro/src/constants/index.ts
+++ astro/src/constants/index.ts
@@ -1,5 +1,5 @@
-export const GITHUB_URL = "https://github.com/Fukupuro";
-export const X_URL = "https://x.com/fukupuro2023";
+export const GITHUB_URL = "https://github.com/Fukupro2023";
+export const X_URL = "https://x.com/fukupro2023";
 
 export * from "./path.ts";
 export * from "./blogMocks.ts";

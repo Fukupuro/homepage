b6418f42d909d70d301325d197e90aa03c818f51
itojum <itojum1230@gmail.com>
2026-02-04 22:39:19 +0900

    add: Preact導入

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,7 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()],
+    integrations: [tailwind(), icon(), preact({ compat: true })],
 });

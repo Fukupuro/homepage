a86ad6e226bc7e65b0b958c74f1b92a416fee20e
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-09 01:32:35 +0900

    fix: methodsをgetだけを許可

--- cms/config/initializers/cors.rb
+++ cms/config/initializers/cors.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "/api/*",
       headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      methods: [ :get, :options ],
       credentials: false
   end
 end

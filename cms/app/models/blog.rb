b74b54e92bed1377673aca7bdcae576994292fe7
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-11 13:56:33 +0900

    feat: バリデーションを追加

--- cms/app/models/blog.rb
+++ cms/app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
   has_many :blog_tags, dependent: :destroy
   has_many :tags, through: :blog_tags
+
+  has_one_attached :header_image
+  has_many_attached :content_images
+
+  validates :title, presence: true
+  validates :description, presence: true
 end

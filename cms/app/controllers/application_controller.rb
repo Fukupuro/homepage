e85fd0efd28150f1e0f1f832226c0dfb86a13f90
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-13 01:41:51 +0900

    update: ベーシック認証を導入

--- cms/app/controllers/application_controller.rb
+++ cms/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  http_basic_authenticate_with(
+    name: ENV.fetch("BASIC_AUTH_USER", "admin"),
+    password: ENV.fetch("BASIC_AUTH_PASSWORD", "password"),
+    except: []
+  )
 end

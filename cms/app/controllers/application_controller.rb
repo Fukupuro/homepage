48eb43a16a256492bb03a096f03294148a7fb28f
蓮華丸 <167959774+Rengemaru@users.noreply.github.com>
2026-02-14 01:20:59 +0900

    fix: 本番環境で環境変数が必須となるように修正

--- cms/app/controllers/application_controller.rb
+++ cms/app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   http_basic_authenticate_with(
-    name: ENV.fetch("BASIC_AUTH_USER", "admin"),
-    password: ENV.fetch("BASIC_AUTH_PASSWORD", "password"),
+    name: ENV.fetch("BASIC_AUTH_USER") { Rails.env.development? ? "admin" : raise("BASIC_AUTH_USER must be set") },
+    password: ENV.fetch("BASIC_AUTH_PASSWORD") { Rails.env.development? ? "password" : raise("BASIC_AUTH_PASSWORD must be set") },
     except: []
   )
 end
